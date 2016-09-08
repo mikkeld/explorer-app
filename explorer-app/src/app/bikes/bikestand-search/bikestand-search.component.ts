c3e7d3ecfadb853dd5e0b576eeacf28a009d0ae9
Mikkel Dengsoe <mikkelden@gmail.com>
2016-09-08 17:28:44 +0100

    Bike search initialized

---
+++ explorer-app/src/app/bikes/bikestand-search/bikestand-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bikestand-search',
+  templateUrl: 'bikestand-search.component.html',
+  styleUrls: ['bikestand-search.component.css']
+})
+export class BikestandSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ explorer-app/src/app/bikes/bikestand-search/index.ts
@@ -0,0 +1,1 @@
+export * from './bikestand-search.component';
