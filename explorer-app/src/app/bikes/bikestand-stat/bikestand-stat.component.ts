e918d860dde2c628a97f70508a7ea9c0c6740ddd
Mikkel Dengsoe <mikkelden@gmail.com>
2016-09-22 19:54:46 +0100

    Pre chart integration

--- explorer-app/src/app/bikes/bikestand-stat/bikestand-stat.component.ts
+++ explorer-app/src/app/bikes/bikestand-stat/bikestand-stat.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { BikestandStatService } from './bikestand-stat.service';
+import { BikeStand } from '../bike';
 
 @Component({
   selector: 'app-bikestand-stat',
   templateUrl: 'bikestand-stat.component.html',
-  styleUrls: ['bikestand-stat.component.css']
+  styleUrls: ['bikestand-stat.component.css'],
+  providers: [BikestandStatService]
 })
 export class BikestandStatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private bikestandStatService: BikestandStatService) { }
+  @Input()
+  currentBikestand: BikeStand;
+
+  bikestandStat: Object[];
 
   ngOnInit() {
+    this.bikestandStatService.getBikestandStat(1)
+      .subscribe(result => {
+        console.log(result);
+        // this.bikestandStatService = result;
+      })
   }
 
 }
--- explorer-app/src/app/bikes/bikestand-stat/bikestand-stat.service.ts
+++ explorer-app/src/app/bikes/bikestand-stat/bikestand-stat.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Injectable()
 export class BikestandStatService {
 
-  constructor() { }
+  constructor(
+    private af: AngularFire,
+    private http: Http
+  ) { }
+
+  getBikestandStat(id: number): Observable<any> {
+    return this.af.database.list(`/time/${id}`)
+      .map(data => {
+        return data.map(res => {
+          return res.available_bikes;
+        })
+      });
+      // .map(day => {
+      //   day.map(minute => {
+      //     return {minute: minute.timestamp, avaliableBikes: minute.available_bikes}
+      //   })
+      // })
+  }
 
 }
